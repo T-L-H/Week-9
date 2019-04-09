04e9762c7f16ed290249e8591976d38ea4e18ab0
User <User@DEV-022778.OnBase.net>
2019-04-09 19:40:38 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Week_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> toDoList = new List<string>();
+                while (true)
+                {
+                {
+                    
+                }
+                    {
+                        Console.WriteLine("Welcome to your list!");
+                        string command = Console.ReadLine();
+                        if(command == "Add")
+                        {
+                            Console.WriteLine("enter text!");
+                            string item = Console.ReadLine();
+                            toDoList.Add(item);
+                        }
+
+                        else if(command == "View"){
+                         if(toDoList.Count == 0){
+                              Console.WriteLine("You have no items in the list");
+                         }
+                           Console.WriteLine(toDoList.Count);
+                            ViewList(toDoList);
+                           
+                        
+                        }
+
+                        
+            
+
+                            else if(command == "Exit"){
+                                break;
+                            }
+
+                    }
+                }
+        }
+    public static void ViewList(List<string> list){
+        foreach(string A in list){
+            Console.WriteLine("-" + A);
+        }
+    }
+}
+}
